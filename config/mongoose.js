c3488fa1a820e9d0e618b464bc998855fe471780
rudra1341 <rudras1341@gmail.com>
2025-07-23 18:22:29 +0530

    deleted unecessary files

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,8 +3,8 @@ const debuglog=require("debug")("development:mongooseconfig");
 require('dotenv').config();
 const url=process.env.db_url;
 
-mongoose.connect(process.env.localdb_url);
-// mongoose.connect(url);
+// mongoose.connect(process.env.localdb_url);
+mongoose.connect(url);
 const db = mongoose.connection;
 
 db.on("error",function(err){
